404eb657be370e97ffe399b8379299c3e0327239
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-02-01 16:50:28 +0700

    Update UI Scripts: use Resources

--- Assets/Scripts/UI/PlayUI.cs
+++ Assets/Scripts/UI/PlayUI.cs
@@ -1,15 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PlayUI : MonoBehaviour {
     public void OnClickPlayMenu() {
+        AudioManager.instance.PlaySound("buttonClick");
         ResetPlayerPrefs();
         SceneManager.LoadScene(1);
     }
-    public void OnClickLevelMenu(int level) {
-        LevelManager.CurrLevel = level-1;
-        SceneManager.LoadScene("Level " + level);
-    }
-
     public void ResetPlayerPrefs() {
         PlayerPrefs.DeleteAll();
         PlayerPrefs.Save();
--- Assets/Scripts/UI/UIInGame.cs
+++ Assets/Scripts/UI/UIInGame.cs
@@ -1,19 +1,27 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class UIInGame : MonoBehaviour {
-    [Header("Game Objects")]
-    [SerializeField] GameObject pauseUI;
-    [SerializeField] GameObject winUI;
-    [SerializeField] GameObject gameOverUI;
+    [Header("UI")]
+    [SerializeField] GameObject pauseButton;
+    [SerializeField] RectTransform pauseUI;
+    [SerializeField] RectTransform winUI;
+    [SerializeField] RectTransform gameOverUI;
+    [SerializeField] RectTransform[] Stars;
+    [Header("Text")]
     [SerializeField] TextMeshProUGUI timerText;
-    [Header("Timer")]
-    [SerializeField] float remainingTime = 0f;
+    [SerializeField] TextMeshProUGUI recordText;
+    [SerializeField] private float bounceHeight = 30f;
+    [SerializeField] private float bounceDuration = 0.5f;
+    [SerializeField] private float delayBetweenStars = 0.2f;
+
+    public static bool isCounting = false;
+    #region innit & Lifecycle
     private void Start() {
-        if (pauseUI != null) {
-            pauseUI.SetActive(false);
-        }
+        this.ResetPauseUI();
+        CalculateTime();
     }
     private void OnEnable() {
         GameManager.OnWinGame += OnWinGame;
@@ -24,34 +32,95 @@ public class UIInGame : MonoBehaviour {
         GameManager.OnGameOver -= OnGameOver;
     }
     private void Update() {
-        remainingTime = Mathf.Max(remainingTime - Time.deltaTime, 0f);
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        if(remainingTime==0) GameManager.OnGameOverInvoke();
+        if (isCounting) {
+            this.CalculateTime();
+        }
     }
+    #endregion
+    private void ResetPauseUI() {
+        pauseButton.SetActive(true);
+        pauseUI.anchoredPosition = new Vector2(0, -Screen.height);
+        pauseUI.gameObject.SetActive(false);
+    }
+    private void CalculateTime() {
+        timerText.text = GameManager.instance.CalTime();
+        if(GameManager.currRemainingTime <= 10f) timerText.color = Color.red;
+    }
+
     public void OnRestartPress() {
+        AudioManager.instance.PlaySound("buttonClick");
+        GameManager.TogglePause();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        GameManager.TogglePause();
     }
-    public void OnPauseGamePress() {
-        pauseUI.SetActive(true);
+
+    public void OnPausePress() {
+        AudioManager.instance.PlaySound("buttonClick");
+        pauseButton.SetActive(false);
+        pauseUI.gameObject.SetActive(true);
         GameManager.TogglePause();
+
+        pauseUI.DOAnchorPos(Vector3.zero, 1f).SetEase(Ease.OutQuad).SetUpdate(true);
+
+        pauseUI.localScale = Vector3.one * 0.6f;
+        pauseUI.DOScale(Vector3.one, 1f).SetEase(Ease.OutBack).SetUpdate(true);
     }
+
     public void OnResumeGamePress() {
-        pauseUI.SetActive(false);
-        GameManager.TogglePause();
+        AudioManager.instance.PlaySound("buttonClick");
+        pauseButton.SetActive(true);
+        pauseUI.DOAnchorPos(new Vector2(0, -Screen.height), 0.5f)
+            .SetEase(Ease.InQuad)
+            .SetUpdate(true).OnComplete(() => {
+                pauseUI.gameObject.SetActive(false);
+                GameManager.TogglePause();
+            });
     }
-    public void OnWinGame() { 
-        winUI.SetActive(true);
+
+    public void OnWinGame() {
+        pauseButton.SetActive(false);
+        winUI.gameObject.SetActive(true);
+        recordText.text = timerText.text;
+
+        winUI.localScale = Vector3.one * 0.6f;
+        winUI.DOScale(Vector3.one, 1f).SetEase(Ease.OutBack).SetUpdate(true);
+
+        AnimateStars();
+
         GameManager.TogglePause();
+        LevelHandler.UnlockNextLevel();
     }
+
+    private void AnimateStars() {
+        for (int i = 0; i < Stars.Length; i++) {
+            RectTransform star = Stars[i];
+            float initialY = star.anchoredPosition.y;
+            foreach (var obj in Stars) {
+                if (obj != null) obj.gameObject.SetActive(true);
+            }
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(star.DOAnchorPosY(initialY + bounceHeight, bounceDuration).SetEase(Ease.OutQuad)).SetUpdate(true);
+            sequence.Append(star.DOAnchorPosY(initialY, bounceDuration).SetEase(Ease.InQuad)).SetUpdate(true);
+            sequence.SetDelay(i * delayBetweenStars).SetLoops(-1, LoopType.Yoyo).SetUpdate(true);
+        }
+    }
+
     public void OnGameOver() {
-        gameOverUI.SetActive(true);
+        pauseButton.SetActive(false);
+        gameOverUI.gameObject.SetActive(true);
+
+        gameOverUI.localScale = Vector3.one * 0.6f;
+        gameOverUI.DOScale(Vector3.one, 1f).SetEase(Ease.OutBack).SetUpdate(true);
+
         GameManager.TogglePause();
+        HeartsSystem.LoseHeart();
     }
-    public void OnExitLevel() {
+
+    public void OnExitLevelPress() {
+        AudioManager.instance.PlaySound("buttonClick");
+        DOTween.Kill(pauseUI);
+        DOTween.Kill(winUI);
+        DOTween.Kill(gameOverUI);
         SceneManager.LoadScene(1);
-        Time.timeScale = 1;
+        GameManager.TogglePause();
     }
 }
