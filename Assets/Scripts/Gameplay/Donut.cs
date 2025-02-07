353224e17ab0d7b14d3194dfb23c06c0aea25277
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-02-07 11:45:24 +0700

    Update scripts: Donut
    - xoá hàm không cần thiết

--- Assets/Scripts/Gameplay/Donut.cs
+++ Assets/Scripts/Gameplay/Donut.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Donut : MonoBehaviour {
     public static Donut instance;
     private Rigidbody2D rb;
-    [SerializeField] float _time;
-
+    private void Update() {
+        rb.AddForce(rb.velocity.normalized * 0.5f, ForceMode2D.Force);
+    }
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
         if(instance==null) instance = this;
     }
     public void SetDonutGravity(float _scale) {
