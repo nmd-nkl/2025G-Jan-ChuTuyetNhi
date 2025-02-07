99a10f217885021d932d3d74052d27b92940b6e1
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-02-07 11:45:56 +0700

    Update cs: PipeRotater
    - xoá hàm không cần thiết

--- Assets/Scripts/Gameplay/PipeRotater.cs
+++ Assets/Scripts/Gameplay/PipeRotater.cs
@@ -6,25 +6,17 @@ public class PipeRotater : MonoBehaviour {
     [SerializeField] private float rotateAngle = 90f;
     private float targetAngle;
     private bool isRotating;
-    private bool isPaused;
 
-    private void OnEnable() {
-        GameManager.OnPauseStateChanged += HandlePauseStateChanged;
-    }
-    private void OnDisable() {
-        GameManager.OnPauseStateChanged -= HandlePauseStateChanged;
-    }
+    
     private void Start() {
         targetAngle = transform.eulerAngles.z;
     }
     private void OnMouseDown() {
-        if (!isPaused && !isRotating) {
+        if (!GameManager.isPaused && !isRotating) {
             targetAngle = (transform.eulerAngles.z + rotateAngle) % 360;
             StartCoroutine(RotatePipe());
         }
     }
-    private void HandlePauseStateChanged(bool paused) => isPaused = paused;
-
     private IEnumerator RotatePipe() {
         isRotating = true;
         while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle)) > 0.1f) {
