87d6168a2295b5d50d557f712860bc9789bcce8f
Chu Tuyết Nhi <chunhi.dev@gmail.com>
2025-02-16 19:06:54 +0700

    Add cs: Editor: ResetPlayerPrefs

---
+++ Assets/Scripts/Editor/PlayerPrefsReset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PlayerPrefsResetTool : EditorWindow {
+    [MenuItem("Tools/PlayerPrefs Reset Tool")]
+    public static void ShowWindow() {
+        GetWindow<PlayerPrefsResetTool>("PlayerPrefs Reset");
+    }
+
+    private void OnGUI() {
+        GUILayout.Label("Reset PlayerPrefs", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Reset PlayerPrefs", GUILayout.Height(30))) {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+            Debug.Log("PlayerPrefs đã được reset!");
+            
+        }
+    }
+}
